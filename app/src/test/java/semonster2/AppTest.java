b69e27786b41d3744fb0d4460310796087fe530f
kamakura <kamakura@git.com>
2023-07-03 10:06:03 +0900

    getGreetingの変更

--- app/src/test/java/semonster2/AppTest.java
+++ app/src/test/java/semonster2/AppTest.java
@@ -7,8 +7,14 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    // @Test public void appHasAGreeting() {
+    //     App classUnderTest = new App();
+    //     assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    // }
+
+    @Test
+    public void getGreeting() {
+      App classUnderTest = new App();
+      assertNotNull("こんにちは SEMonster", classUnderTest.getGreeting());
     }
-}
+  }
