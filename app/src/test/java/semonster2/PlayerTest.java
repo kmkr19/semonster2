110a9465948721df41c61d925bc29e29d82473d2
kamakura <kamakura@git.com>
2023-07-03 13:20:41 +0900

    PlayerデバッグとSeMonsterGameの追加

--- app/src/test/java/semonster2/PlayerTest.java
+++ app/src/test/java/semonster2/PlayerTest.java
@@ -3,10 +3,24 @@ package semonster2;
 import org.junit.Test;
 import org.junit.Assert;
 
+import java.util.LinkedList;
+
 public class PlayerTest {
     @Test
     public void testtoString(){
-        Player player = new Player(0,0);
-        Assert.assertEquals("スライム2:レア度[2]\n",player.toString());
+        LinkedList<Integer> monsterList = new LinkedList<>();
+        monsterList.add(4);
+        monsterList.add(4);
+        monsterList.add(3);
+        monsterList.add(3);
+        monsterList.add(2);
+        monsterList.add(2);
+        monsterList.add(1);
+        monsterList.add(1);
+        monsterList.add(0);
+        monsterList.add(0);
+        String name = "user"; 
+        Player player = new Player(monsterList, name);
+        Assert.assertEquals("Deck:user\n",player.toString());
     }
 }
